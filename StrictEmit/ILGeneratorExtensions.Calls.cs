2d0df54d13d68136cf81ceec92e8c8aad5f3dc80
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-03-12 22:59:58 +0100

    Add utility functions for calling methods and constructors on objects.
    These overloads are based on a modified version of the library created
    by gerard on the C# server. Thanks!

--- StrictEmit/ILGeneratorExtensions.Calls.cs
+++ StrictEmit/ILGeneratorExtensions.Calls.cs
@@ -33,6 +33,97 @@ namespace StrictEmit
     public static partial class ILGeneratorExtensions
     {
         /// <summary>
+        /// Calls the constructor on the given type indicated by the (optional) parameter type set.
+        /// </summary>
+        /// <typeparam name="T">The type in which the constructor exists.</typeparam>
+        /// <param name="il">The generator where the IL is to be emitted.</param>
+        /// <param name="parameterTypes">The parameter types of the constructor.</param>
+        public static void EmitCallConstructorDirect<T>([NotNull] this ILGenerator il, params Type[] parameterTypes)
+        {
+            var type = typeof(T);
+            var constructor = type.GetConstructor(parameterTypes);
+
+            if (constructor is null)
+            {
+                throw new EntryPointNotFoundException("No constructor with those parameters could be found.");
+            }
+
+            il.EmitCallDirect(constructor);
+        }
+
+        /// <summary>
+        /// Calls a late-bound constructor on the given type indicated by the (optional) parameter type set.
+        /// </summary>
+        /// <typeparam name="T">The type in which the constructor exists.</typeparam>
+        /// <param name="il">The generator where the IL is to be emitted.</param>
+        /// <param name="parameterTypes">The parameter types of the constructor.</param>
+        public static void EmitCallConstructorVirtual<T>([NotNull] this ILGenerator il, params Type[] parameterTypes)
+        {
+            var type = typeof(T);
+            var constructor = type.GetConstructor(parameterTypes);
+
+            if (constructor is null)
+            {
+                throw new EntryPointNotFoundException("No constructor with those parameters could be found.");
+            }
+
+            il.EmitCallVirtual(constructor);
+        }
+
+        /// <summary>
+        /// Calls the method in the given type, indicated by its name (and optionally, its parameters).
+        /// </summary>
+        /// <typeparam name="T">The type in which the method exists.</typeparam>
+        /// <param name="il">The generator where the IL is to be emitted.</param>
+        /// <param name="functionName">The name of the function.</param>
+        /// <param name="parameterTypes">The parameter types of the function.</param>
+        public static void EmitCallDirect<T>
+        (
+            [NotNull] this ILGenerator il,
+            [NotNull] string functionName,
+            params Type[] parameterTypes
+        )
+        {
+            var type = typeof(T);
+            var method = type.GetMethod(functionName, parameterTypes);
+
+            if (method is null)
+            {
+                throw new EntryPointNotFoundException("No method with that name and parameter combination could be found.");
+            }
+
+            il.EmitCallDirect(method);
+        }
+
+        /// <summary>
+        /// Calls a late-bound method on an object, pushing the return value onto the evaluation stack. The method is
+        /// resolved in the given type, indicated by its name (and optionally, its parameters).
+        /// </summary>
+        /// <typeparam name="T">The type in which the method exists.</typeparam>
+        /// <param name="il">The generator where the IL is to be emitted.</param>
+        /// <param name="functionName">The name of the function.</param>
+        /// <param name="parameterTypes">The parameter types of the function.</param>
+        public static void EmitCallVirtual<T>
+        (
+            [NotNull] this ILGenerator il,
+            [NotNull] string functionName,
+            params Type[] parameterTypes
+        )
+        {
+            var type = typeof(T);
+            var method = parameterTypes.Length > 0
+                ? type.GetMethod(functionName, parameterTypes)
+                : type.GetMethod(functionName);
+
+            if (method is null)
+            {
+                throw new EntryPointNotFoundException("No method with that name and type combination could be found.");
+            }
+
+            il.EmitCallVirtual(method);
+        }
+
+        /// <summary>
         /// Calls the method indicated by the passed method descriptor.
         /// </summary>
         /// <param name="il">The generator where the IL is to be emitted.</param>
