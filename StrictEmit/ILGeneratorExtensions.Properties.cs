9019ee5d435439ac7eac5090c33793fbb48c5b2a
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-10-11 17:41:16 +0200

    Add property overloads that take binding flags.

--- StrictEmit/ILGeneratorExtensions.Properties.cs
+++ StrictEmit/ILGeneratorExtensions.Properties.cs
@@ -18,6 +18,7 @@
 //
 
 using System;
+using System.Reflection;
 using System.Reflection.Emit;
 using JetBrains.Annotations;
 
@@ -59,6 +60,39 @@ namespace StrictEmit
         /// <summary>
         /// Pops the value on the evaluation stack, setting the named property's value to it.
         /// </summary>
+        /// <param name="il">The generator where the IL is to be emitted.</param>
+        /// <param name="containingType">The type in which the property is defined.</param>
+        /// <param name="propertyName">The name of the property.</param>
+        /// <param name="flags">The binding flags to use.</param>
+        [PublicAPI]
+        public static void EmitSetProperty
+        (
+            [NotNull] this ILGenerator il,
+            [NotNull] Type containingType,
+            [NotNull] string propertyName,
+            BindingFlags flags
+        )
+        {
+            var property = containingType.GetProperty(propertyName, flags);
+            if (property is null)
+            {
+                throw new ArgumentException
+                (
+                    $"The type {containingType.Name} did not contain a property with that name."
+                );
+            }
+
+            if (!property.CanWrite)
+            {
+                throw new ArgumentException($"The property \"{property.Name}\" doesn't have a setter.");
+            }
+
+            il.EmitCallDirect(property.SetMethod);
+        }
+
+        /// <summary>
+        /// Pops the value on the evaluation stack, setting the named property's value to it.
+        /// </summary>
         /// <typeparam name="T">The type in which the property is defined.</typeparam>
         /// <param name="il">The generator where the IL is to be emitted.</param>
         /// <param name="propertyName">The name of the property.</param>
@@ -81,6 +115,39 @@ namespace StrictEmit
         )
         {
             var property = containingType.GetProperty(propertyName);
+            if (property is null)
+            {
+                throw new ArgumentException
+                (
+                    $"The type {containingType.Name} did not contain a property with that name."
+                );
+            }
+
+            if (!property.CanRead)
+            {
+                throw new ArgumentException($"The property \"{property.Name}\" doesn't have a getter.");
+            }
+
+            il.EmitCallDirect(property.GetMethod);
+        }
+
+        /// <summary>
+        /// Gets the value of the named property, placing it onto the evaluation stack.
+        /// </summary>
+        /// <param name="il">The generator where the IL is to be emitted.</param>
+        /// <param name="containingType">The type in which the property is defined.</param>
+        /// <param name="propertyName">The name of the property.</param>
+        /// <param name="flags">The binding flags to use.</param>
+        [PublicAPI]
+        public static void EmitGetProperty
+        (
+            [NotNull] this ILGenerator il,
+            [NotNull] Type containingType,
+            [NotNull] string propertyName,
+            BindingFlags flags
+        )
+        {
+            var property = containingType.GetProperty(propertyName, flags);
             if (property is null)
             {
                 throw new ArgumentException
